5f8f8ff8bb4f8947c5cd9960905786aec3fa5d1b
jcmenaartero <jcmenaartero@gmail.com>
2021-12-16 02:07:26 +0100

    2021/12/15

--- dockerfiles/ubuntu/mysql/start-mysql.sh
+++ dockerfiles/ubuntu/mysql/start-mysql.sh
@@ -6,7 +6,7 @@ sed -i 's/# pid-file/pid-file/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i 's/# socket/socket/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i 's/# port/port/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i 's/# datadir/datadir/' /etc/mysql/mysql.conf.d/mysqld.cnf
-#sed -i 's/= 127.0.0.1/= 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i 's/= 127.0.0.1/= 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
 
 mkdir /var/run/mysql
 chown -R mysql:mysql /var/run/mysql
